8667582a0717aae2f251649ac764c3df999fa255
d3nik <113850410+d3nik@users.noreply.github.com>
2026-01-24 19:53:11 +0200

    patch 2.2
    – tags get request implementation.

--- index.js
+++ index.js
@@ -43,8 +43,12 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     }
 });
 
+
+app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
+app.get('/posts/tags', PostController.getLastTags);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationError, PostController.createPost);
 app.delete('/posts/:id', checkAuth, PostController.removePost);
 app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationError, PostController.updatePost);
